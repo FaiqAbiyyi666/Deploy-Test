e823d983596a72891e71e975fa8709070f948c87
Faiq Al Abiyyi <abiyyiaja@gmail.com>
2024-05-03 20:28:16 +0700

    fix problem in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(logger("dev"));
 app.use(express.json());
 
-app.get("/", (req, req) =>
+app.get("/", (res, req) =>
   res.json({ status: true, message: "Hello World!", data: null })
 );
 const routes = require("./routes");
